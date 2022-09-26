c3cf751cbda6e7b55c94b8854078c056d2e71c5f
unknown <cogjava2565@iiht.tech>
2022-09-26 12:53:21 +0530

    eureka server implemented and all client register with eureka

---
+++ src/main/java/com/durgesh/LmsEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.durgesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LmsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durgesh/LmsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
